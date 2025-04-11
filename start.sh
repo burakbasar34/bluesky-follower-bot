c15e0f091ce23d959671200124c8750c062574c0
Burak Başar <burakbasar@stu.aydin.edu.tr>
2025-04-11 03:26:38 +0300

    sh update

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-source /bluesky-bot/venv/bin/activate
+cd /bluesky-bot
+source venv/bin/activate
+export PYTHONPATH=$(pwd)
 exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker -b 127.0.0.1:8000 main:app
